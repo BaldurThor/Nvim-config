3f39227543a988a7cee5cfb2bb8b72b90233275d
BaldurThor <baldurthor96@gmail.com>
2024-11-01 01:06:37 +0000

    mini

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,14 +6,14 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     mappings = vim.g.have_nerd_font,
-    spec = {
-        { '<Leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
     },
   },
 }
