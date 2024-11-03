f2de89e96f8dd34f0e4c274577ef9f673b02168c
BaldurThor <baldurthor96@gmail.com>
2024-11-03 20:12:57 +0000

    cleanup

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -37,7 +37,7 @@ return { -- Autoformat
 			rust = { "rustfmt" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
 }
