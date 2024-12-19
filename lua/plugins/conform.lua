abbf23c10446ce2c9f0349dd2afe05c6e8f5ce72
BaldurThor <baldurthor96@gmail.com>
2024-12-19 18:17:39 +0000

    Clangd

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -39,6 +39,7 @@ return { -- Autoformat
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			glsl = { "glsl_analyzer" },
+			cpp = { "clangd" },
 		},
 	},
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -178,6 +178,7 @@ return {
 			pyright = {},
 			rust_analyzer = {},
 			glsl_analyzer = {},
+			clangd = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
