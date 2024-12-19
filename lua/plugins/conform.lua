49cb876687e8ddbe9e903f79fa17be4a01bb194e
BaldurThor <baldurthor96@gmail.com>
2024-12-19 18:25:48 +0000

    Clang-format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -39,7 +39,7 @@ return { -- Autoformat
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			glsl = { "glsl_analyzer" },
-			cpp = { "clangd" },
+			cpp = { "clang-format" },
 		},
 	},
 }
