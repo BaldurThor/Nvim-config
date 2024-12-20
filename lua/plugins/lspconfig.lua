2e1c1fdcbcbfc07eee88c7c2dbc5a3e7ae8748e1
BaldurThor <baldurthor96@gmail.com>
2024-12-20 15:02:29 +0000

    csharp_ls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -179,6 +179,7 @@ return {
 			rust_analyzer = {},
 			glsl_analyzer = {},
 			clangd = {},
+			csharp_ls = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
