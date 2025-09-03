072c7e78c930c07c48de17867cc30382d61d6a02
BaldurThor <baldurthor96@gmail.com>
2025-09-03 15:36:45 +0000

    errors

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
-vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.opt.clipboard = "unnamedplus"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
@@ -15,4 +15,4 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
-vim.o.tabstop = 4
+vim.opt.tabstop = 4
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+-- lua/plugins/treesitter.lua
 return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = { "OXY2DEV/markview.nvim" },
@@ -5,12 +6,27 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			highlight = { enable = true }, -- Enable syntax highlighting
-			indent = { enable = true }, -- Enable indentation
-			ensure_installed = { "lua", "vimdoc", "python" }, -- Example: always install these parsers
-			auto_install = true, -- Automatically install missing parsers on file open
+			-- A list of parser names, or "all"
+			ensure_installed = { "lua", "vimdoc", "python" },
+
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+
+			-- Automatically install missing parsers when entering buffer
+			auto_install = true,
+
+			-- List of parsers to ignore installing
+			ignore_install = {},
+
+			highlight = {
+				enable = true,
+				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				-- Using this option may slow down your editor, and you may see some duplicate highlights.
+				-- Instead of true it can also be a list of languages
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
 		})
 	end,
-
-	-- ... All other options.
 }
