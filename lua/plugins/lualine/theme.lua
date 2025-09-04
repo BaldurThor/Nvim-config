c9d7f6c7dd7b63d4eb717324846c4c5bfd477dcd
BaldurThor <baldurthor96@gmail.com>
2025-09-04 14:47:53 +0000

    Lualine ready

---
+++ lua/plugins/lualine/theme.lua
@@ -0,0 +1,49 @@
+local lualine = require("lualine")
+
+local ignore_fts = { "snacks_dashboard", "toggleterm", "NvimTree" }
+
+local exclude = function()
+	return vim.fn.empty(vim.fn.expand("%:t")) ~= 1 and not vim.tbl_contains(ignore_fts, vim.bo.filetype)
+end
+
+local get_filename = function()
+	if vim.bo.filetype == ignore_fts[1] then
+		return [[Dashboard]]
+	elseif vim.bo.filetype == ignore_fts[2] then
+		return [[Terminal]]
+	elseif vim.bo.filetype == ignore_fts[3] then
+		return [[NvimTree]]
+	else
+		return require("lualine.components.filename"):new():update_status()
+	end
+end
+
+local get_location = function()
+	if vim.tbl_contains(ignore_fts, vim.bo.filetype) then
+		return [[]]
+	else
+		return require("lualine.components.location")()
+	end
+end
+
+-- Config
+local config = {
+	options = { theme = "tomorrow_night" },
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { { "branch", cond = exclude }, { "diff", cond = exclude }, { "diagnostics", cond = exclude } },
+		lualine_c = { get_filename },
+		lualine_x = { { "encoding", cond = exclude }, { "fileformat", cond = exclude }, { "filetype", cond = exclude } },
+		lualine_y = { { "progress", cond = exclude } },
+		lualine_z = { get_location },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { get_filename },
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+}
+lualine.setup(config)
