931b2061727023d46c0cf6454ace56395eebeb56
BaldurThor <baldurthor96@gmail.com>
2025-09-10 09:38:36 +0000

    Revert "oil"
    This reverts commit 745285ad1b9e96edd7571213014e4507b1418607.

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- File Browser
---vim.keymap.set("n", "\\", "<cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "\\", "<cmd>NvimTreeToggle<CR>")
 
 --[[
 -- Remote sshfs
@@ -19,5 +19,3 @@ vim.keymap.set("n", "<leader>re", api.edit, { desc = "Edit" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 
 vim.keymap.set("n", "<leader>df", ":FormatBufferToggle<CR>")
-
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
--- lua/plugins/lualine/theme.lua
+++ lua/plugins/lualine/theme.lua
@@ -1,6 +1,6 @@
 local lualine = require("lualine")
 
-local ignore_fts = { "snacks_dashboard", "toggleterm", "trouble", "oil" }
+local ignore_fts = { "snacks_dashboard", "toggleterm", "NvimTree", "trouble" }
 
 local exclude = function()
 	return vim.fn.empty(vim.fn.expand("%:t")) ~= 1 and not vim.tbl_contains(ignore_fts, vim.bo.filetype)
@@ -12,9 +12,9 @@ local get_filename = function()
 	elseif vim.bo.filetype == ignore_fts[2] then
 		return [[Terminal]]
 	elseif vim.bo.filetype == ignore_fts[3] then
-		return [[Trouble]]
+		return [[NvimTree]]
 	elseif vim.bo.filetype == ignore_fts[4] then
-		return [[Oil]]
+		return [[Trouble]]
 	else
 		return require("lualine.components.filename"):new():update_status()
 	end
@@ -35,11 +35,7 @@ local config = {
 		lualine_a = { "mode" },
 		lualine_b = { { "branch", cond = exclude }, { "diff", cond = exclude }, { "diagnostics", cond = exclude } },
 		lualine_c = { get_filename },
-		lualine_x = {
-			{ "encoding", cond = exclude },
-			{ "fileformat", cond = exclude },
-			{ "filetype", cond = exclude },
-		},
+		lualine_x = { { "encoding", cond = exclude }, { "fileformat", cond = exclude }, { "filetype", cond = exclude } },
 		lualine_y = { { "progress", cond = exclude } },
 		lualine_z = { get_location },
 	},
